ec6eedba56d2f475fa4bfcbb67b204f4688e58f9
Justin Taylor <justin2taylor@gmail.com>
2022-01-20 11:33:33 -0800

    0.0.2 & bin fix

--- src/lib/zxp.ts
+++ src/lib/zxp.ts
@@ -1,12 +1,10 @@
 import * as os from "os";
 import * as path from "path";
-import { execSync } from "child_process";
+import * as child_process from "child_process";
+const { execSync } = child_process;
 
 import { removeIfExists, safeCreate, log } from "./lib";
 import { CEP_Config } from "../cep-config";
-import * as fs from "fs";
-
-const { existsSync } = fs;
 
 export const signZXP = (config: CEP_Config, input: string, zxpDir: string) => {
   const zxpCmd = os.platform() == "win32" ? `ZXPSignCmd` : `./ZXPSignCmd`;
@@ -14,7 +12,6 @@ export const signZXP = (config: CEP_Config, input: string, zxpDir: string) => {
   const data = config.zxp;
   const output = path.join(zxpDir, `${name}.zxp`);
   const certPath = `${name}-cert.p12`;
-
   const signPrepStr = `${zxpCmd} -selfSignedCert ${data.country} ${data.province} ${data.org} ${name} ${data.password} ${certPath}`;
   const signStr = `${zxpCmd} -sign "${input}" "${output}" ${certPath} ${data.password} -tsa ${data.tsa}`;
   const cwdDir = path.join(__dirname, "..", "bin");
