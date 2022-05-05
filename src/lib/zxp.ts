b938962aa81484b2d7bfdce6ff94ef6628160f2d
Justin Taylor <justin.taylor@buck.tv>
2022-05-05 14:09:19 -0700

    fix paths with spaces

--- src/lib/zxp.ts
+++ src/lib/zxp.ts
@@ -16,9 +16,9 @@ export const signZXP = (
   const name = config.id;
   const data = config.zxp;
   const output = path.join(zxpDir, `${name}.zxp`);
-  const certPath = path.join(tmpDir, `${name}-cert.p12`);
-  const signPrepStr = `${zxpCmd} -selfSignedCert ${data.country} ${data.province} ${data.org} ${name} ${data.password} ${certPath}`;
-  const signStr = `${zxpCmd} -sign "${input}" "${output}" ${certPath} ${data.password} -tsa ${data.tsa}`;
+  const certPath = path.join(tmpDir, `${name}-cert  `);
+  const signPrepStr = `${zxpCmd} -selfSignedCert ${data.country} ${data.province} ${data.org} ${name} ${data.password} "${certPath}"`;
+  const signStr = `${zxpCmd} -sign "${input}" "${output}" "${certPath}" ${data.password} -tsa ${data.tsa}`;
   const cwdDir = path.join(__dirname, "..", "bin");
 
   removeIfExists(output);
