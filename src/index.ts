77c831b5908a500acbb0602ff555d71a18433135
Justin Taylor <justin2taylor@gmail.com>
2022-09-09 12:51:16 -0700

    added ponyfill module

--- src/index.ts
+++ src/index.ts
@@ -438,10 +438,16 @@ export const jsxInclude = ({
       bundle: { [key: string]: { code: string } }
     ) => {
       const esFile = Object.keys(bundle).pop() as keyof object;
-      const core = [...foundIncludes, bundle[esFile].code];
+      const core = [
+        "// EXTENDSCRIPT INCLUDES --- START",
+        ...foundIncludes,
+        "// EXTENDSCRIPT INCLUDES --- END",
+        bundle[esFile].code,
+      ];
       if (iife) {
         const banner = `(function (${globalThis}) {`;
         const footer = "})(this);";
+        console.log("test"), console.log("test");
         bundle[esFile].code = [banner, ...core, footer].join("\r");
       } else {
         bundle[esFile].code = core.join("\r");
@@ -488,6 +494,86 @@ export const jsxInclude = ({
           s.overwrite(start, end, "");
         });
       }
+      return {
+        code: s.toString(),
+        map: s.generateMap({
+          source: id,
+          file: `${id}.map`,
+          includeContent: true,
+        }),
+      };
+    },
+  };
+};
+
+interface PonyFillItem {
+  find: string;
+  replace: string;
+  inject: string;
+}
+export const jsxPonyfill = (extraPonyfills?: PonyFillItem[]): Plugin | any => {
+  let usedPonyfills = new Set<PonyFillItem>();
+  let ponyfills: PonyFillItem[] = [
+    {
+      find: "Object.freeze",
+      replace: "__objectFreeze",
+      inject: `function __objectFreeze(obj) { return obj; }`,
+    },
+    {
+      find: "Array.isArray",
+      replace: "__isArray",
+      inject: `function __isArray(arr) { try { return arr instanceof Array; } catch (e) { return false; } };`,
+    },
+  ];
+  if (extraPonyfills) {
+    ponyfills = [...ponyfills, ...extraPonyfills];
+  }
+  return {
+    name: "extendscript-ponyfill-resolver",
+    generateBundle: (
+      output: any,
+      bundle: { [key: string]: { code: string } }
+    ) => {
+      const esFile = Object.keys(bundle).pop() as keyof object;
+
+      let ponyfillStr = [
+        `// EXTENDSCRIPT PONYFILLS --- START`,
+        ponyfills.map((p) => p.inject).join("\r"),
+        `// EXTENDSCRIPT PONYFILLS --- END`,
+      ].join("\r");
+
+      const core = [ponyfillStr, bundle[esFile].code];
+      bundle[esFile].code = core.join("\r");
+    },
+    renderChunk: (code: string, chunk: any) => {
+      const id = chunk.fileName;
+      const s = new MagicString(code);
+      console.log("Ponyfill Time");
+
+      ponyfills.map((pony) => {
+        const regexp = new RegExp(pony.find, "g");
+        const gen = code.matchAll(regexp);
+        console.log("GEN", gen);
+        if (gen) {
+          const matches = [...gen];
+          console.log("FOUND!", pony.find);
+          matches.map((match) => {
+            usedPonyfills.add(pony);
+            const index = match.index;
+            const length = match[0].length;
+            if (index) {
+              console.log("REPLACING :: ", index, index + length);
+              s.overwrite(
+                index,
+                index + length,
+                pony.replace
+                // text
+              );
+            }
+          });
+        }
+      });
+
       return {
         code: s.toString(),
         map: s.generateMap({
